f73fba6096724c7cc5f787a7e306f759dd032f15
fmcfall <60740193+fmcfall@users.noreply.github.com>
2022-04-13 13:04:30 +0100

    manually isolated periodic orbit and calulated T and a fixed phase condition

--- predPreyFunc.py
+++ predPreyFunc.py
@@ -6,14 +6,15 @@ import matplotlib.pyplot as plt
 def dXdt(X, t=0):
 
     a = 1
-    b = 0.5
+    b = 0.15
     d = 0.1
 
     return np.array([X[0] * (1 - X[0]) - (a * X[0] * X[1]) / (d + X[0]),
                     (b * X[1]) * (1 - (X[1] / X[0]))])
 
-t = np.linspace(0,30,1000)
-X0 = np.array([10, 5])
+t = np.linspace(0,200,1000)
+X0 = np.array([0.6, 0.3])
+print(X0)
 X, infodict = odeint(dXdt, X0, t, full_output=True)
 print(infodict['message'])
 
@@ -26,3 +27,6 @@ plt.legend(loc='best')
 plt.xlabel('time')
 plt.ylabel('population')
 plt.show()
+
+# T = 25
+# t(0) = 
