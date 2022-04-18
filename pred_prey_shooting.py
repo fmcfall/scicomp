54da84bf103c00b6d04b3d9faa8c0128e0566be4
fmcfall <60740193+fmcfall@users.noreply.github.com>
2022-04-18 13:37:47 +0100

    function now returns u0 with initial y values

--- pred_prey_shooting.py
+++ pred_prey_shooting.py
@@ -15,7 +15,10 @@ predator-prey shooting method
 '''
 
 def get_ode_data(ode, u0, args):
-
+    '''
+    gathers data for ode using solve_ivp
+    '''
+    
     # unpack arguments
     x0 = u0[:-1]
     t = u0[-1]
@@ -29,27 +32,29 @@ def get_ode_data(ode, u0, args):
     return np.asarray([y_data, t_data], dtype=object)
 
 def update_u0(y_data, t_data):
+    '''
+    returns array for u0 including y values and period
+    '''
     
     # argrelectrema returns array of indices of maxima 
-    maxima = argrelmax(y_data)[0]
+    maxima = argrelmax(y_data[0])[0]
 
     # loop finds two maxima that are the same (within 1e-4 of each other)
     c = 0
-    i1 = maxima[c]
-    i2 = maxima[c+1]
-    while not isclose(y_data[i1], y_data[i2], rel_tol=1e-4):
+    i1, i2 = maxima[c], maxima[c+1]
+    while not isclose(y_data[0,i1], y_data[0,i2], rel_tol=1e-4):
         c += 1
+        i1, i2 = maxima[c], maxima[c+1]
 
     # calculates period between two maxima
     period = t_data[i2] - t_data[i1]
-    u0 = [y_data[i2], t_data[i2], period]
     
-    return u0
+    return np.append(y_data[:,i2], period)
 
 def main():
 
     args = np.array([1, 0.2, 0.1])
-    u0 = [1, 0.5, 200]
+    u0 = np.array([1, 0.5, 200])
     p = get_ode_data(dXdt, u0, args)
 
     y = p[0]
@@ -57,7 +62,7 @@ def main():
     predator = y[1]
     t = p[1]
 
-    print(get_period(predator, t))
+    print(update_u0(y, t))
 
     f1 = plt.figure()
     plt.plot(t, prey, 'r-', label='Prey')
