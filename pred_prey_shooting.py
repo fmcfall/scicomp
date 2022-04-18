6afb10de5fe5e347e7e394bfd7e0bdc0efca2389
fmcfall <60740193+fmcfall@users.noreply.github.com>
2022-04-18 12:27:10 +0100

    get period function

--- pred_prey_shooting.py
+++ pred_prey_shooting.py
@@ -1,12 +1,11 @@
+from re import M
 import numpy as np
 from scipy.integrate import odeint, solve_ivp
 import matplotlib.pyplot as plt
+from scipy.signal import argrelmax
+from math import isclose
 
-def dXdt(t, X):
-
-    a = 1
-    b = 0.15
-    d = 0.1
+def dXdt(t, X, a, b, d):
 
     return np.array([X[0] * (1 - X[0]) - (a * X[0] * X[1]) / (d + X[0]),
                     (b * X[1]) * (1 - (X[1] / X[0]))])
@@ -27,17 +26,33 @@ def get_ode_data(ode, u0, args):
 
     return np.array([y_data, t_data])
 
+def get_period(y_data, t_data):
+    
+    # argrelectrema returns array of indices of maxima 
+    maxima = argrelmax(y_data)[0]
+
+    c = 0
+    while not isclose(y_data[maxima[c]], y_data[maxima[c+1]], rel_tol=1e-4):
+        c += 1
+
+    print(y_data[maxima[c]],y_data[maxima[c+1]])
+
+    period = t_data[maxima[c+1]] - t_data[maxima[c]]
+    
+    return period
+
 def main():
 
-    args = np.array([])
-    u0 = [0.5, 0.5, 100]
+    args = np.array([1, 0.2, 0.1])
+    u0 = [1, 0.5, 200]
     p = get_ode_data(dXdt, u0, args)
-    print(p)
 
     y = p[0]
     prey = y[0]
     predator = y[1]
     t = p[1]
+
+    print(get_period(predator, t))
 
     f1 = plt.figure()
     plt.plot(t, prey, 'r-', label='Prey')
