d5175986d18540709de4efbdbfeee1cfdf28cf50
fmcfall <60740193+fmcfall@users.noreply.github.com>
2022-04-21 11:02:04 +0100

    switched to pytest

--- test_shooting.py
+++ test_shooting.py
@@ -1,16 +1,13 @@
-import unittest
+import pytest
 import numpy as np
 from all_ode import hopf_bifurcation
 from shooting_method import shooting
 
-class TestShootingMethod(unittest.TestCase):
+class TestShootingMethod():
     def test_output_length(self):
         args = np.array([0.5, -1])
         u0 = [1.5, 0, 20]
         sol = shooting(hopf_bifurcation, u0, args=args)
-        self.assertEqual(len(sol),len(u0))
-
-if __name__ == '__main__':
-    unittest.main()
+        assert len(sol) == len(u0)
 
     
