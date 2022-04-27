a6f8ca31fb7587ad364cb56ca84bfeacf71e1bb7
fmcfall <60740193+fmcfall@users.noreply.github.com>
2022-04-27 15:32:30 +0100

    error plot func for different methods

--- solve_ode.py
+++ solve_ode.py
@@ -9,9 +9,9 @@ def euler_step(t, X, func, args, h):
 def rk4_step(t, X, func, args, h):
 
     k1 = func(t, X, *args)
-    k2 = func(t+h/2, X+(h*k1)/2, args)
-    k3 = func(t+h/2, X+(h*k2)/2, args)
-    k4 = func(t+h, X+(h*k3), args)
+    k2 = func(t+h/2, X+(h*k1)/2, *args)
+    k3 = func(t+h/2, X+(h*k2)/2, *args)
+    k4 = func(t+h, X+(h*k3), *args)
     k = (h * (k1+2*k2+2*k3+k4))/6
 
     return  np.array([X + k, t + h])
@@ -22,7 +22,7 @@ def solve_to(func, x0, args, t0, tend, deltat_max, method):
     x = x0
 
     while t < tend:
-        if t + deltat_max > tend:
+        if tend < t + deltat_max:
             deltat_max = tend - t
         x, t = method(t, x, func, args, deltat_max)
         
@@ -30,21 +30,22 @@ def solve_to(func, x0, args, t0, tend, deltat_max, method):
 
 def solve_ode(func, x0, args, tspan, deltat_max, method):
 
-    solution = np.zeros([tspan.size,x0.size])
+    solution = [[] for x in x0]
     t0 = tspan[0]
 
-    for i, tend in enumerate(tspan):
+    for tend in tspan:
         sol = solve_to(func, x0, args, t0, tend, deltat_max, method)
-        solution[i,:] = sol[0]
+        for i, x in enumerate(sol[0]):
+            solution[i].append(x)
         x0 = sol[0]
         t0 = sol[1]
 
     return solution
 
 def plot_solution(tspan, solution):
-
-    for (index, sol) in enumerate(solution.T):
-	    plt.plot(tspan, sol, label='x{}'.format(index+1))
+    
+    for i, sol in enumerate(solution):
+        plt.plot(tspan, sol, label='x{}'.format(i+1))
     
     plt.ylabel('x')
     plt.xlabel('time (s)')
@@ -53,12 +54,33 @@ def plot_solution(tspan, solution):
 
 def get_error(tspan, solution, exact_func, exact_args):
 
-    exact_solution = exact_func(tspan, exact_args)
-    abs_error = np.absolute(solution - exact_solution)
-    print(abs_error)
+    exact_solution = np.array([exact_func(tspan, exact_args) for t in tspan])
+    abs_error = np.absolute(np.subtract(solution, exact_solution))
 
-    return sum(abs_error)
+    return np.sum(abs_error)
 
+def get_error_list(tspan, func, x0, args, exact_func, exact_args, deltat_min, deltat_max, deltat_num, method):
+
+    delta_t_list = np.logspace(deltat_min, deltat_max, deltat_num)
+    error_list = []
+    for delta_t in delta_t_list:
+        sol = solve_ode(func, x0, args, tspan, delta_t, method)
+        error_list.append(get_error(tspan, sol, exact_func, exact_args))
+
+    return error_list, delta_t_list
+
+def plot_errors(tspan, func, x0, args, exact_func, exact_args, deltat_min, deltat_max, deltat_num, methods):
+
+    for m in methods:
+        method = m
+        error_list, deltat_list = get_error_list(tspan, func, x0, args, exact_func, exact_args, deltat_min, deltat_max, deltat_num, method)
+        plt.loglog(deltat_list, error_list, label=method.__name__)
+
+    plt.ylabel('Error')
+    plt.xlabel('Step size')
+    plt.legend()
+    plt.show()
+    
 def main():
 
     func = simple
@@ -67,11 +89,12 @@ def main():
     exact_func = simple_exact
     exact_args = np.array((1))
     deltat_max = 0.01
-    tspan = np.linspace(0,50,50)
-    method = euler_step
+    tspan = np.linspace(0,2,20)
+    methods = [euler_step, rk4_step]
+    method = rk4_step
     sol = solve_ode(func, x0, args, tspan, deltat_max, method)
-    err = get_error(tspan, sol, exact_func, exact_args)
-    print(err)
+    plot_solution(tspan, sol)
+    plot_errors(tspan, func, x0, args, exact_func, exact_args, -5, -1, 20, methods)
 
 if __name__ == "__main__":
 
