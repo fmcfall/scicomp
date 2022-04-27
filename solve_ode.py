fa157f984fd02eb71a23ad2a195a3a02995486c9
fmcfall <60740193+fmcfall@users.noreply.github.com>
2022-04-27 03:15:11 +0100

    method changes and plot sol func

--- solve_ode.py
+++ solve_ode.py
@@ -1,64 +1,67 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from all_ode import lokta_volterra
+
+def euler_step(t, X, func, args, h):
+
+    return np.array([X + h * func(t, X, *args), t + h])
+
+def rk4_step(t, X, func, args, h):
+
+    k1 = func(t, X, *args)
+    k2 = func(t+h/2, X+(h*k1)/2, args)
+    k3 = func(t+h/2, X+(h*k2)/2, args)
+    k4 = func(t+h, X+(h*k3), args)
+    k = (h * (k1+2*k2+2*k3+k4))/6
+
+    return  np.array([X + k, t + h])
+
+def solve_to(func, x0, args, t0, tend, deltat_max, method):
+
+    t = t0
+    x = x0
+
+    while t < tend:
+        if t + deltat_max > tend:
+            deltat_max = tend - t
+        x, t = method(t, x, func, args, deltat_max)
+    return x, t
+
+def solve_ode(func, x0, args, tspan, deltat_max, method):
+
+    solution = np.zeros([len(tspan),len(x0)])
+    t0 = tspan[0]
+
+    for i, tend in enumerate(tspan):
+        sol = solve_to(func, x0, args, t0, tend, deltat_max, method)
+        solution[i,:] = sol[0]
+        x0 = sol[0]
+        t0 = sol[1]
+
+    return solution
+
+def plot_solution(tspan, solution):
+
+    for (index, sol) in enumerate(solution.T):
+	    plt.plot(tspan, sol, label='x{}'.format(index+1))
+    
+    plt.ylabel('Value')
+    plt.xlabel('time (s)')
+    plt.legend()
+    plt.show()
 
 def main():
 
-    def func(X,t):
-        
-        x, v = X
-        dxdt = v
-        dvdt = -x
-
-        return np.array([dxdt, dvdt])
-    
-    t = np.linspace(0,1,10)
-    X = [1, 0]
-
-    print(solve_ode(func, X, t, method='rk4'))
-
-def euler_step(func, x, t, h):
-
-    return x + h * func(x, t)
-
-def rk4_step(func, x, t, h):
-
-    k1 = h * func(x, t)
-    k2 = h * func(x+k1/2, t+h/2)
-    k3 = h * func(x+k2/2, t+h/2)
-    k4 = h * func(x+k3, t+h/2)
-    k = (k1+2*k2+2*k3+k4)/6
-
-    return x + k
-
-def solve_to(func, x0, t0, tend, deltat_max):
-
-    x = x0 # initial x
-    t = t0
-    h = tend / deltat_max
-
-    for i in range(deltat_max):
-        x = euler_step(func, x, t, h)
-        t += h
-
-    return x
-
-def solve_ode(func, x0, t, method="euler"):
-
-    solution = np.zeros((len(t),len(x0)))
-    solution[0,:] = x0
-
-    if method == "euler":
-        
-        for i in range(len(t)-1):
-
-            solution[i+1] = euler_step(func, solution[i], t[i], t[i+1]-t[i])
-
-    elif method == "rk4":
-
-        for i in range(len(t)-1):
-
-            solution[i+1] = rk4_step(func, solution[i], t[i], t[i+1]-t[i])
-
-    return solution
+    func = lokta_volterra
+    u0 = np.array((0.5, 1, 100))
+    args = np.array((1, 0.2, 0.1))
+    u0 = u0[:-1]
+    deltat_max = 0.01
+    tspan = np.linspace(0,50,50)
+    method = euler_step
+    sol = solve_ode(func, u0, args, tspan, deltat_max, method)
+    print(sol)
+    plot_solution(tspan, sol)
 
 if __name__ == "__main__":
 
