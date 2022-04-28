35df4052a6a4e9a48129188be4c23b91f869d877
fmcfall <60740193+fmcfall@users.noreply.github.com>
2022-04-28 11:51:45 +0100

    pdes

--- all_pde.py
+++ all_pde.py
@@ -5,7 +5,7 @@ def uI(x, L):
     y = np.sin(np.pi*x/L)
     return y
 
-def uExact(x, t, kappa, L):
+def uExact(x, L, t, kappa):
     # The exact solution
     y = np.exp(-kappa*(np.pi**2/L**2)*t)*np.sin(np.pi*x/L)
     return y
