dd5f94f7e97ffe258f2bcbc3ec42bd50258e0c20
fmcfall <60740193+fmcfall@users.noreply.github.com>
2022-04-29 05:47:00 +0100

    test

--- test_shooting.py
+++ test_shooting.py
@@ -1,19 +1,19 @@
 import pytest
 import numpy as np
-from all_ode import hopf_bifurcation
+from all_ode import HopfBifurcation
 from shooting import shooting, get_ode_data
 
 class TestShootingMethod():
     def test_output_length(self):
         args = np.array([0.5, -1])
         u0 = [1.5, 0, 20]
-        sol = shooting(hopf_bifurcation, u0, args=args)
+        sol = shooting(HopfBifurcation.func, u0, args=args)
         assert len(sol) == len(u0)
 
     def test_data_length(self):
         args = np.array([0.5, -1])
         u0 = [1.5, 0, 20]
-        y, t = get_ode_data(hopf_bifurcation, u0, args=args)
+        y, t = get_ode_data(HopfBifurcation.func, u0, args=args)
         for i in y:
             assert len(i) == len(t)
     
